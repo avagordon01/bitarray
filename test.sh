e79b8c8ec5148c7abb105f55e64027a688de3470
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-11-20 19:19:45 +0000

    change buildtype to debugoptimized

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 if [ ! -d out ]; then
     CXX=clang++ \
     meson out \
-    -Db_sanitize=address -Db_lundef=false
+    -Db_sanitize=address -Db_lundef=false \
+    --buildtype debugoptimized
 fi
 ninja -C out &&
 out/bitarray-test
