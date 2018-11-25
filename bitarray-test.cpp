1a2391f8f895f6dc5f355271aeccbcc49206cf68
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-11-25 23:14:13 +0000

    test formatting

--- bitarray-test.cpp
+++ bitarray-test.cpp
@@ -12,31 +12,36 @@ struct g {
 };
 
 int main() {
-    using T = bitarray<129>;
-    assert((T{-1LLU, -1LLU, -1LLU}).all());
-    assert((T{0, 0, 0}).none());
-    assert(!(T{0, 0, 0}).any());
-    assert((T{1, 0, 0}).any());
-    assert((T{1, 2, 3}).count() == 4);
-
-    assert((T{1, 2, 3}).count() == 4);
-
-    assert((T{0, 1 << 10, 0}).count_trailing_zeros() == 64 + 10);
-    assert((T{0, 1 << 10, 0}).count_leading_zeros() == 64 + 63 - 10);
-
-    /*
     {
+        using T = bitarray<129>;
+        assert((T{-1LLU, -1LLU, -1LLU}).all());
+        assert((T{0, 0, 0}).none());
+        assert(!(T{0, 0, 0}).any());
+        assert((T{1, 0, 0}).any());
+        assert((T{1, 2, 3}).count() == 4);
+
+        assert((T{1, 2, 3}).count() == 4);
+
+        assert((T{0, 1 << 10, 0}).count_trailing_zeros() == 64 + 10);
+        std::cout << (T{~0LLU, ~0LLU, ~0LLU}).count_leading_zeros() << std::endl;
+        assert((T{0, 1 << 10, 0}).count_leading_zeros() == 129 - 64 - 11);
+    }
+
+    {
+        /*
         bitarray<192> input{~0LLU, 0LLU, 0LLU}, output{};
         std::cout << input << std::endl;
         input = input >> 63;
         std::cout << input << std::endl;
+        */
     }
-    */
 
-    bitarray<64> input{~0LLU};
-    std::cout << input << std::endl;
-    auto output = input.interleave<2>(input);
-    std::cout << output << std::endl;
+    {
+        bitarray<64> input{0xffLLU << 32 | 0x55LLU};
+        std::cout << input << std::endl;
+        auto output = input.interleave<2>(input);
+        std::cout << output << std::endl;
+    }
 
     return 0;
 }
